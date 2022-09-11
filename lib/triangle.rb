32b481775083bf750bd5b9be03a4d6ab4b5ebbef
Casey Tansey <ctansey12600@outlook.com>
2022-09-11 18:54:59 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if self.length1 > 0 && self.length2 > 0 && self.length3 > 0
+      if self.length1 + self.length2 > self.length3 && self.length2 + self.length3 > self.length1 && self.length3 + self.length1 > self.length2
+        if self.length1 == self.length2 && self.length2 == self.length3
+        :equilateral
+        elsif self.length1 == self.length2 || self.length2 == self.length3 || self.length3 == self.length1
+        :isosceles
+        elsif self.length1 != self.length2 || self.length2 != self.length3 || self.length3 != self.length1
+        :scalene
+        else
+          raise TriangleError
+        end
+      else
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+    def message
+    end
+  end
 end
